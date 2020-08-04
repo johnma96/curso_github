a483b0f577746ad3ce3e4280b7419bd6a5a47f3d
Mario Montoya <jmmontoyaz96@gmail.com>
2020-08-04 12:34:17 -0500

    Agrego la clase Humano

--- archivo1.py
+++ archivo1.py
@@ -10,3 +10,38 @@ import .archivo2
 
 
 print('Generando un nuevo cambio en master y creando rama funciones2')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Humano():
+    def __init__(self, nombre, apellido, profesion):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.profesion = profesion
+
+    def hablar(self, frase):
+        print('Don {} dice {}'.format(self.nombre, frase))
